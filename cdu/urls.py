d59d899e6760fbc32cd566d689eebbd3a0a0c898
Antoine Andrieu <antoine@scircula.com>
2020-04-10 08:54:52 +0200

    Migrate to django 3

--- cdu/urls.py
+++ cdu/urls.py
@@ -13,10 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.conf.urls import url, include, re_path, path
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^accounts/', include('django_warrant.urls',namespace='dw'))
+    path('admin/', admin.site.urls),
+    re_path('accounts/', include(('django_warrant.urls', 'dw'), namespace='dw'))
 ]
